f31419ae2ba45127b3f5ce8fd1576ec04c1f3d7b
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2023-02-27 23:42:34 -0500

    moved part2 to openmp subdir

--- assignment_3/openmp/job.sh
+++ assignment_3/openmp/job.sh
@@ -6,10 +6,10 @@
 #SBATCH --cpus-per-task=8 #(limit is 32)
 #SBATCH --mem-per-cpu=600mb
 #SBATCH -t 00:05:00
-#SBATCH -o out.txt
+#SBATCH -o out_200x200_8.txt
 #SBATCH -e err.txt
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=8
 #(NRA, NCA_RB, NCB)
-./matmult 60 12 10
-#(./matmult 200 200 200)
+#(./matmult 60 12 10)
+./matmult 200 200 200
 
